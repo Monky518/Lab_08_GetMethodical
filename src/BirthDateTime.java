747479f245b7d58192b44049c2d5cae2182f96eb
Monky518 <zoeyrizor@gmail.com>
2023-10-19 15:16:48 -0400

    Finish changes for today

---
+++ src/BirthDateTime.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BirthDateTime
+{
+    public static void main(String[] args)
+    {
+        // Use the getRangedInt method to input:
+        // the year (1950-2015)
+        // month (1-12)
+        // Day*
+        // hours (1–24)
+        // Minutes (0-59)
+        //
+        // Note: Use a switch() conditional selector structure to limit the user to the correct number of days for the month they were born in
+        // For instance if they were born in Feb [1-29], Oct [1-31]
+        // HINT: there are only 3 groups here not 12 different ones!
+
+        Scanner scan = new Scanner(System.in);
+        int year = 0;
+        int month = 0;
+        int day = 0;
+        int hour = 0;
+        int min = 0;
+
+        year = SafeInput.getRangedInt(scan, "Enter your birth year", 1950, 2015);
+        month = SafeInput.getRangedInt(scan, "Enter your birth month", 1, 12);
+        // 4 6 9 11 : 30
+        // 1 3 5 7 8 10 12 : 31
+        switch (month)
+        {
+            case 2:
+                day = SafeInput.getRangedInt(scan, "Enter your birth day", 1, 29);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = SafeInput.getRangedInt(scan, "Enter your birth day", 1, 30);
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                day = SafeInput.getRangedInt(scan, "Enter your birth day", 1, 31);
+                break;
+        }
+
+    }
+}
---
+++ src/FavNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FavNumbers
+{
+    // Prompt the user to enter their favorite integer and then their favorite double
+    // Use the unconstrained getInt and getDouble methods for this
+    //
+    // Really you only have two tests here, non-numeric input and valid numeric input for each method
+
+    public static void main(String[] args)
+    {
+        int favInt = 0;
+        double favDouble = 0.0;
+        Scanner scan = new Scanner(System.in);
+
+        // if (n % 2) == 1, then odd
+
+        favInt = SafeInput.getInt(scan, "Enter your favorite int number");
+        favDouble = SafeInput.getDouble(scan, "Enter your favorite double number");
+
+        System.out.println("You said your favorite numbers are " + favInt + " and " + favDouble);
+    }
+}
